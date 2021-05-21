2e90ee74fb753172592ec2174323f30d90e69212
Rockstedy <Rockstedy69@tut.by>
2021-05-21 19:29:05 +0300

    one

---
+++ script2.js
@@ -0,0 +1,12 @@
+window.addEventListener("load", function() {
+	
+	let p = document.getElementById("p");
+
+	p.innerHTML = " New Script JS";
+	
+	
+	alert("Hi everybody!");
+	alert("Hi there!");
+	alert("Hi BUDDY!");
+	
+}, false)
